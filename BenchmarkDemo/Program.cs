60c69c9d18cdc4de799a91f66f5f5cef02b39160
Brian Neal <brianbneal@gmail.com>
2025-01-28 08:17:06 -0600

    wip

--- BenchmarkDemo/Program.cs
+++ BenchmarkDemo/Program.cs
@@ -1,7 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using System.Security.Cryptography;
 
 [MemoryDiagnoser]
 public class Program
@@ -12,33 +10,33 @@ public class Program
     }
 }
 
-[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
-[SimpleJob(RuntimeMoniker.NetCoreApp30)]
-[SimpleJob(RuntimeMoniker.NativeAot80)]
-[SimpleJob(RuntimeMoniker.Mono)]
-[RPlotExporter]
-public class Md5VsSha256
-{
-    private SHA256 sha256 = SHA256.Create();
-    private MD5 md5 = MD5.Create();
-    private byte[] data;
+//[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+//[SimpleJob(RuntimeMoniker.NetCoreApp30)]
+//[SimpleJob(RuntimeMoniker.NativeAot80)]
+//[SimpleJob(RuntimeMoniker.Mono)]
+//[RPlotExporter]
+//public class Md5VsSha256
+//{
+//    private SHA256 sha256 = SHA256.Create();
+//    private MD5 md5 = MD5.Create();
+//    private byte[] data;
 
-    [Params(1000, 10000)]
-    public int N;
+//    [Params(1000, 10000)]
+//    public int N;
 
-    [GlobalSetup]
-    public void Setup()
-    {
-        data = new byte[N];
-        new Random(42).NextBytes(data);
-    }
+//    [GlobalSetup]
+//    public void Setup()
+//    {
+//        data = new byte[N];
+//        new Random(42).NextBytes(data);
+//    }
 
-    [Benchmark]
-    public byte[] Sha256() => sha256.ComputeHash(data);
+//    [Benchmark]
+//    public byte[] Sha256() => sha256.ComputeHash(data);
 
-    [Benchmark]
-    public byte[] Md5() => md5.ComputeHash(data);
-}
+//    [Benchmark]
+//    public byte[] Md5() => md5.ComputeHash(data);
+//}
 
 public class IntroMemoryRandomization
 {
